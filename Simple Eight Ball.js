2ae183ab36778be4213f76cc1b8ec54d92777acf
Marvis-C <96761431+Marvis-C@users.noreply.github.com>
2022-12-02 22:15:58 -0500

    Add files via upload

---
+++ Simple Eight Ball.js
@@ -0,0 +1,35 @@
+userName = '';
+userName ? console.log(`Hello, ${userName}!`) : console.log('Hello!');
+
+Question = 'Should I order pizza?';
+console.log(Question)
+
+randomNumber = Math.floor(Math.random() * 8);
+
+eightBall = randomNumber;
+switch (eightBall) {
+  case 0 :
+    console.log('It is certain') ;
+    break ;
+  case 1 :
+    console.log('It is decidely so') ;
+    break ; 
+  case 2 :
+    console.log('Reply hazy try again') ;
+    break ;
+  case 3 :
+    console.log('Cannot predict now') ;
+    break ;
+  case 4 :
+    console.log('Do not count on it') ;
+    break ;
+  case 5 :
+    console.log('My sources say no') ;
+    break ;
+  case 6 :
+    console.log('Outlook not so good') ;
+    break ;
+  case 7 :
+    console.log('Signs point to yes') ;
+    break ;
+}
